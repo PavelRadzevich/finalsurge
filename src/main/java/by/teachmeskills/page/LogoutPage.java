723cd71008b0520dd3f382a234e451030713d3bb
PavelRadzevich <pavel.radzevich@gmail.com>
2023-04-28 03:34:42 +0300

    FS-01: Added LogoutPage.

---
+++ src/main/java/by/teachmeskills/page/LogoutPage.java
@@ -0,0 +1,9 @@
+package by.teachmeskills.page;
+
+import com.codeborne.selenide.SelenideElement;
+import static com.codeborne.selenide.Selenide.$x;
+
+public class LogoutPage {
+    public final SelenideElement accountLoginLnk = $x("//a[text()='Account Login']");
+
+}
