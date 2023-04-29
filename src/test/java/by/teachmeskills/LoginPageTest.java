54560e73d9b0387298028e8cc4385f29e3e24619
PavelRadzevich <pavel.radzevich@gmail.com>
2023-04-29 23:30:43 +0300

    FS-01: Edited LoginPageTest. invocationCount = 1

--- src/test/java/by/teachmeskills/LoginPageTest.java
+++ src/test/java/by/teachmeskills/LoginPageTest.java
@@ -12,7 +12,7 @@ import org.testng.annotations.Test;
 public class LoginPageTest extends BaseTest {
     private Logger log = LogManager.getLogger(LoginPageTest.class);
 
-    @Test(invocationCount = 5,
+    @Test(invocationCount = 1,
             groups = {"negative"})
     public void checkWithoutParameters() {
         LoginPage loginPage = new LoginPage().openLink().loginAs("", "");
@@ -29,7 +29,7 @@ public class LoginPageTest extends BaseTest {
 
     @Test(dataProvider = "any-login", dataProviderClass = DPClass.class,
             groups = {"negative"},
-            invocationCount = 5)
+            invocationCount = 1)
     public void checkWithAnyParameters(String login, String pass) {
         log.info("Run checkAnyValues with {}, {}", login, pass);
         LoginPage loginPage = new LoginPage().openLink().loginAs(login, pass);
@@ -40,7 +40,7 @@ public class LoginPageTest extends BaseTest {
                         .because("error text is invalid"));
     }
 
-    @Test(invocationCount = 5,
+    @Test(invocationCount = 1,
             groups = {"regression"})
     public void checkValidLoginNoPass() {
         LoginPage loginPage = new LoginPage().openLink().loginValidLoginNoPassword();
@@ -51,7 +51,7 @@ public class LoginPageTest extends BaseTest {
                         .because("error text is invalid"));
     }
 
-    @Test(invocationCount = 5,
+    @Test(invocationCount = 1,
             groups = {"regression"})
     public void checkValidLoginWithWrongPass() {
         LoginPage loginPage = new LoginPage().openLink().loginValidLoginWrongPassword();
@@ -62,7 +62,7 @@ public class LoginPageTest extends BaseTest {
                         .because("error text is invalid"));
     }
 
-    @Test(invocationCount = 5,
+    @Test(invocationCount = 1,
             groups = {"negative"})
     public void checkUserLock() {
         LoginPage loginPage = new LoginPage().openLink().loginWithMultipleAttempts();
@@ -71,7 +71,7 @@ public class LoginPageTest extends BaseTest {
                         .because("error text is invalid"));
     }
 
-    @Test(invocationCount = 5,
+    @Test(invocationCount = 1,
             groups = {"smoke"})
     public void loginValidUser() {
         CalendarPage calPage = new LoginPage().openLink().loginValidUser();
