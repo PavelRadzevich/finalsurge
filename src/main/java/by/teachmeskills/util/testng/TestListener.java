efbb898e291bb73b4a52f4c347a789c19bf8bac9
PavelRadzevich <pavel.radzevich@gmail.com>
2023-04-29 23:28:08 +0300

    FS-01: Edited TestListener

--- src/main/java/by/teachmeskills/util/testng/TestListener.java
+++ src/main/java/by/teachmeskills/util/testng/TestListener.java
@@ -1,5 +1,8 @@
 package by.teachmeskills.util.testng;
 
+import by.teachmeskills.page.LoginPage;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
@@ -7,22 +10,22 @@ import org.testng.ITestResult;
 import java.util.concurrent.TimeUnit;
 
 public class TestListener implements ITestListener {
-
+    private Logger log = LogManager.getLogger(LoginPage.class);
 
     @Override
     public void onTestStart(ITestResult result) {
-        System.out.printf("======================================== STARTING TEST %s ========================================%n", result.getName());
+        log.info("======================================== STARTING TEST {} ========================================", result.getName());
     }
 
     @Override
     public void onTestSuccess(ITestResult result) {
-        System.out.printf("======================================== FINISHED TEST %s Duration: %ss ========================================%n", result.getName(),
+        log.info("======================================== FINISHED TEST {} Duration: {}s ========================================", result.getName(),
                 getExecutionTime(result));
     }
 
     @Override
     public void onTestSkipped(ITestResult result) {
-        System.out.printf("======================================== SKIPPING TEST %s ========================================%n", result.getName());
+        log.info("======================================== SKIPPING TEST {} ========================================", result.getName());
     }
 
     @Override
