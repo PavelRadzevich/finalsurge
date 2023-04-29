258910f7b8279ceffec945fa84cd983df0f7a26a
PavelRadzevich <pavel.radzevich@gmail.com>
2023-04-29 14:40:35 +0300

    FS-01: Edited BaseTest. Added running tests in different browsers.

--- src/test/java/by/teachmeskills/BaseTest.java
+++ src/test/java/by/teachmeskills/BaseTest.java
@@ -1,9 +1,12 @@
 package by.teachmeskills;
 
 import by.teachmeskills.util.PropertiesLoader;
+import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.apache.commons.lang3.StringUtils;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
 import java.util.Properties;
@@ -13,10 +16,13 @@ public class BaseTest {
     Properties properties = PropertiesLoader.loadProperties();
 
     @Parameters("browser")
-    @BeforeClass
-    void setUp() {
+    @BeforeClass(alwaysRun = true)
+    void setUp(@Optional("chrome") String browser) {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
-
+        if (StringUtils.equals(browser, "firefox")) {
+            Configuration.browser = "firefox";
+        } else {
+            Configuration.browser = "chrome";
+        }
     }
-
 }
