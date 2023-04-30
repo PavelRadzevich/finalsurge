556e67b2d18db994605eafc314fc2a4162e501e0
PavelRadzevich <pavel.radzevich@gmail.com>
2023-04-30 04:19:02 +0300

    FS-01: Edited UserProfilePageTest

--- src/test/java/by/teachmeskills/UserProfilePageTest.java
+++ src/test/java/by/teachmeskills/UserProfilePageTest.java
@@ -1,5 +1,26 @@
 package by.teachmeskills;
 
-public class UserProfilePageTest {
+import by.teachmeskills.page.LoginPage;
+import by.teachmeskills.page.UserProfilePage;
+import com.codeborne.selenide.Condition;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.annotations.Test;
 
+public class UserProfilePageTest {
+    private Logger log = LogManager.getLogger(LoginPageTest.class);
+
+    @Test
+    public void checkFieldsFirsLastWithoutValue() {
+        UserProfilePage userProfile = new LoginPage()
+                .openLink()
+                .loginValidUser()
+                .goUserProfile();
+        userProfile.setUserFirstName("")
+                .setUserLastName("").clickSave()
+                .enterValidFNMsg.shouldBe(Condition.text("*Please enter a value for First Name.")
+                        .because("no error message"));
+        userProfile.enterValidLNMsg.shouldBe(Condition.text("*Please enter a value for Last Name.")
+                .because("no error message"));
+    }
 }
