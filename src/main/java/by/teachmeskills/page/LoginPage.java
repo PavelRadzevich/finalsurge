f239f315591fd09fc3fd02dfd9a235df5e4309c3
PavelRadzevich <pavel.radzevich@gmail.com>
2023-04-30 21:19:03 +0300

    FS-01: Edited LoginPage. edited log message and added constructor LoginPage

--- src/main/java/by/teachmeskills/page/LoginPage.java
+++ src/main/java/by/teachmeskills/page/LoginPage.java
@@ -1,10 +1,14 @@
 package by.teachmeskills.page;
 
+import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.SelenideElement;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import javax.security.auth.login.LoginContext;
+
 import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.WebDriverRunner.url;
 
 public class LoginPage extends BasePage {
     public final SelenideElement loginField = $("#login_name");
@@ -13,18 +17,22 @@ public class LoginPage extends BasePage {
     public final SelenideElement passErrMsg = $("form#login-validate label.error[for='login_password']");
     public final SelenideElement invalidCredErrMsg = $("div[class='alert alert-error']");
     public final SelenideElement loginBtn = $x("//button[text()='Login']");
-    public  final String INCORRECT_PASS = "xxx";
+    public final String INCORRECT_PASS = "xxx";
     public final int ATTEMPT_COUNT = 10;
 
     private Logger log = LogManager.getLogger(LoginPage.class);
 
+    public LoginPage(){
+        log.info("Open page {}", properties.getProperty("LOGIN_PAGE"));
+        open(properties.getProperty("LOGIN_PAGE"));
+    }
     public LoginPage loginAs(String login, String pass) {
         loginField.clear();
         loginField.val(login);
         passField.clear();
         passField.val(pass).pressEnter();
-        log.info("---TEST---");
-        return page(this);
+        log.info("Login as LOGIN: {},  PASS: {} ", login, pass);
+        return this;
     }
 
     public LoginPage loginWithMultipleAttempts() {
@@ -34,33 +42,32 @@ public class LoginPage extends BasePage {
             attempt--;
             loginPage.passField.val(INCORRECT_PASS);
             loginBtn.click();
-        } while ( attempt > 0);
-
-        return page(this);
+        } while (attempt > 0);
+        log.info("URL : {}", url().toString());
+        return this;
     }
 
     public LoginPage loginValidLoginNoPassword() {
         LoginPage loginPage = new LoginPage().openLink().loginAs(properties.getProperty("ACCOUNT_LOGIN"), "");
-        log.info("---TEST---");
-        return page(this);
+        log.info("URL : {}", url().toString());
+        return this;
     }
 
     public LoginPage loginValidLoginWrongPassword() {
         LoginPage loginPage = new LoginPage().openLink().loginAs(properties.getProperty("ACCOUNT_LOGIN"), INCORRECT_PASS);
-        log.info("---TEST---");
-        return page(this);
+        log.info("URL : {}", url().toString());
+        return this;
     }
 
     public CalendarPage loginValidUser() {
         LoginPage loginPage = new LoginPage().openLink().loginAs(properties.getProperty("ACCOUNT_LOGIN"),
                 properties.getProperty("ACCOUNT_PASSWORD"));
-        log.info("---TEST---");
-        return page(CalendarPage.class);
+        log.info("URL : {}", url().toString());
+        return new CalendarPage();
     }
 
     public LoginPage openLink() {
-        return page(open(properties.getProperty("LOGIN_PAGE"),
-                LoginPage.class));
+        log.info("URL Open : {}", url().toString());
+        return this;
     }
-
 }
