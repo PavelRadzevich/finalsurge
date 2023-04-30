d3ccd62dc316698542dc5fc1101ee58d06c040d1
PavelRadzevich <pavel.radzevich@gmail.com>
2023-04-30 21:20:58 +0300

    FS-01: Edited UserProfilePage. edited locators

--- src/main/java/by/teachmeskills/page/UserProfilePage.java
+++ src/main/java/by/teachmeskills/page/UserProfilePage.java
@@ -2,6 +2,8 @@ package by.teachmeskills.page;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.SelenideElement;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
@@ -12,90 +14,97 @@ public class UserProfilePage extends BasePage {
     public final SelenideElement profileEditBtn = $x("//span[normalize-space()='Edit Profile']");
     public final SelenideElement profilePictureTitle = $x("//label[text()='Profile Picture']");
     public final SelenideElement profileEditCancelBtn = $("span.dropdown-toggle").$(withText("Cancel"));
-    public final SelenideElement firstNameFld = $("input[name='fname']");
-    public final SelenideElement lastNameFld = $("input[name='lname']");
-    public final SelenideElement birthDayFld = $("input[name='BDay']");
-    public final SelenideElement weightFld = $("input[name='Weight']");
-    public final SelenideElement cityFld = $("input[name='City']");
-    public final SelenideElement zipFld = $("input[name='Zip']");
+    public final SelenideElement firstNameEditFld = $("input[name='fname']");
+    public final SelenideElement lastNameEditFld = $("input[name='lname']");
+    public final SelenideElement birthDayEditFld = $("input[name='BDay']");
+    public final SelenideElement weightEditFld = $("input[name='Weight']");
+    public final SelenideElement cityEditFld = $("input[name='City']");
+    public final SelenideElement zipEditFld = $("input[name='Zip']");
     public final SelenideElement saveChangesBtn = $("input[name='btnSubmit']");
     public final SelenideElement fixErrorsMsg = $x("//div[@class='alert alert-error']");
 
+    public final SelenideElement profileNameFld = $x("//small[@class='muted'][normalize-space()='Name:']/parent::p");
+    public final SelenideElement profileBirthdayFld = $x("//small[@class='muted'][normalize-space()='Birthday:']/parent::p");
+    public final SelenideElement profileWeightFld = $x("//small[@class='muted'][normalize-space()='Weight:']/parent::p");
+    public final SelenideElement profileCityFld = $x("//small[@class='muted'][normalize-space()='City:']/parent::p");
+    public final SelenideElement profileZipFld = $x("//small[@class='muted'][normalize-space()='Zip/Postal Code:']/parent::p");
+
+    private Logger log = LogManager.getLogger(UserProfilePage.class);
 
     public UserProfilePage clickEdit() {
         profileEditBtn.shouldBe(Condition.visible).click();
         profilePictureTitle.shouldBe(Condition.visible.because("profile edit menu not opening"));
-        return page(this);
+        return this;
     }
 
     public UserProfilePage clickSave() {
         saveChangesBtn.shouldBe(Condition.visible).click();
-        return page(this);
+        return this;
     }
 
     public UserProfilePage clickCancel() {
         profileEditCancelBtn.shouldBe(Condition.visible).click();
-        return page(this);
+        return this;
     }
 
     public UserProfilePage setUserFirstName(String firstName) {
-        editField(firstNameFld, firstName);
-        return page(this);
+        editField(firstNameEditFld, firstName);
+        return this;
     }
 
     public String getUserFirstName() {
-        return firstNameFld.val();
+        return firstNameEditFld.val();
     }
 
     public UserProfilePage setUserLastName(String lastName) {
-        editField(lastNameFld, lastName);
-        return page(this);
+        editField(lastNameEditFld, lastName);
+        return this;
     }
 
     public String getUserLastName() {
-        return lastNameFld.val();
+        return lastNameEditFld.val();
     }
 
     public UserProfilePage setUserBirthDay(String birthDay) {
-        editField(birthDayFld, birthDay);
-        return page(this);
+        editField(birthDayEditFld, birthDay);
+        return this;
     }
 
     public String getUserBirthDay() {
-        return birthDayFld.val();
+        return birthDayEditFld.val();
     }
 
     public UserProfilePage setUserWeight(String weight) {
-        editField(weightFld, weight);
-        return page(this);
+        editField(weightEditFld, weight);
+        return this;
     }
 
     public String getUserWeight() {
-        return weightFld.val();
+        return weightEditFld.val();
     }
 
     public UserProfilePage setUserCity(String city) {
-        editField(cityFld, city);
-        return page(this);
+        editField(cityEditFld, city);
+        return this;
     }
 
     public String getUserCity() {
-        return cityFld.val();
+        return cityEditFld.val();
     }
 
     public UserProfilePage setUserZip(String zip) {
-        editField(zipFld, zip);
-        return page(this);
+        editField(zipEditFld, zip);
+        return this;
     }
 
     public String getUserZip() {
-        return zipFld.val();
+        return zipEditFld.val();
     }
 
-    private UserProfilePage editField(SelenideElement element, String value) {
+    private void editField(SelenideElement element, String value) {
         profilePictureTitle.shouldBe(Condition.visible);
         element.shouldBe(Condition.visible).clear();
         element.shouldBe(Condition.visible).val(value);
-        return page(this);
+        log.info("Edit element {}, value {}",element, value);
     }
 }
