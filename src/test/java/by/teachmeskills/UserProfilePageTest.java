3c424e131ea66d19016baba57750e18cb04c9274
PavelRadzevich <pavel.radzevich@gmail.com>
2023-04-30 05:17:06 +0300

    FS-01: Edited UserProfilePageTest. Added tests

--- src/test/java/by/teachmeskills/UserProfilePageTest.java
+++ src/test/java/by/teachmeskills/UserProfilePageTest.java
@@ -7,20 +7,42 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
 
-public class UserProfilePageTest {
+public class UserProfilePageTest extends BaseTest {
     private Logger log = LogManager.getLogger(LoginPageTest.class);
 
-    @Test
+    @Test(groups = {"negative"})
     public void checkFieldsFirsLastWithoutValue() {
         UserProfilePage userProfile = new LoginPage()
                 .openLink()
                 .loginValidUser()
                 .goUserProfile();
-        userProfile.setUserFirstName("")
-                .setUserLastName("").clickSave()
-                .enterValidFNMsg.shouldBe(Condition.text("*Please enter a value for First Name.")
-                        .because("no error message"));
-        userProfile.enterValidLNMsg.shouldBe(Condition.text("*Please enter a value for Last Name.")
-                .because("no error message"));
+        userProfile.clickEdit().setUserFirstName("")
+                   .setUserLastName("").clickSave();
+        userProfile.fixErrorsMsg.shouldBe(Condition.text(" *Please enter a value for First Name.")
+                                                   .because("no error message"));
+        userProfile.fixErrorsMsg.shouldBe(Condition.text(" *Please enter a value for Last Name.")
+                                                   .because("no error message"));
+    }
+
+    @Test(groups = {"negative"})
+    public void checkFieldFirsWithoutValue() {
+        UserProfilePage userProfile = new LoginPage()
+                .openLink()
+                .loginValidUser()
+                .goUserProfile();
+        userProfile.clickEdit().setUserFirstName("").clickSave();
+        userProfile.fixErrorsMsg.shouldBe(Condition.text(" *Please enter a value for First Name.")
+                                                   .because("no error message"));
+    }
+
+    @Test(groups = {"negative"})
+    public void checkFieldLastWithoutValue() {
+        UserProfilePage userProfile = new LoginPage()
+                .openLink()
+                .loginValidUser()
+                .goUserProfile();
+        userProfile.clickEdit().setUserLastName("").clickSave();
+        userProfile.fixErrorsMsg.shouldBe(Condition.text(" *Please enter a value for Last Name.")
+                                                   .because("no error message"));
     }
 }
